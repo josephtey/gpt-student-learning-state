c6f237a56fc29509ccac399d7f256e58e8a26687
Joseph Tey <jjttey@gmail.com>
2023-01-21 19:56:59 -0800

    updated ui

--- src/App.js
+++ src/App.js
@@ -20,16 +20,33 @@ function App() {
   const onChange = (newValue) => {};
 
   return (
-    <div className="flex flex-col">
-      <h1 className="text-center font-bold">learn to code with ai!</h1>
-      <div className="w-200">
+    <div className="flex flex-row h-screen pt-32 gap-4 justify-center bg-stone-100">
+      <div className="w-80 bg-white ml-5 rounded-lg p-4 h-96">
+        <b>Coding Problem:</b>
+        <p>
+          We are looking for the name of an internet host within a string. Find
+          the '.com' in s. Find the series of alphabetic chars or periods before
+          the '.com' with a while loop and return the whole hostname, so 'xx
+          www.foo.com xx' returns 'www.foo.com'. Return the empty string if
+          there is no '.com'. This version has the added complexity of the
+          periods.
+        </p>
+      </div>
+      <div className="flex flex-row bg-stone-100">
         <AceEditor
           mode="python"
           theme="monokai"
           onChange={onChange}
           name="editor"
           editorProps={{ $blockScrolling: true }}
+          style={{
+            "border-radius": "8px",
+            height: "24rem",
+          }}
         />
+      </div>
+      <div className="w-80 bg-white rounded-lg p-4 h-96">
+        <div className="bg-stone-200 rounded-lg w-full h-fit"></div>
       </div>
     </div>
   );
