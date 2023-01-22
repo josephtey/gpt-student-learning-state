2390562cffd15235ba3d7c65a0691b265dfd62c0
Joseph Tey <jjttey@gmail.com>
2023-01-22 02:08:17 -0800

    Fixed cors issue

--- src/gpt3.js
+++ src/gpt3.js
@@ -1,13 +1,17 @@
-import axios from "axios";
-
-const db = axios.create({
-  baseURL: "https://us-central1-gp-learning-state.cloudfunctions.net/",
-});
-
 export async function callGPT3(myPrompt) {
-  const response = await db.post("/open-ai", {
-    prompt: myPrompt,
-  });
+  const response = await fetch(
+    "https://us-central1-gp-learning-state.cloudfunctions.net/open-ai",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: myPrompt,
+      }),
+    }
+  );
+  const data = await response.json();
 
-  return response.response;
+  return data.response;
 }
