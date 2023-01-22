dba79abca84bfd850707247c7792f682ccb66dfe
Joseph Tey <jjttey@gmail.com>
2023-01-21 19:09:21 -0800

    Added gpt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
 import "./App.css";
+import { callGPT3 } from "./gpt3";
+import AceEditor from "react-ace";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  useEffect(() => {
+    const test = async () => {
+      const response = await callGPT3("hello! how are you.");
+
+      console.log(response);
+    };
+
+    test();
+  }, []);
+
+  const onChange = (newValue) => {};
+
+  return (
+    <div className="flex">
+      <p className="text-center">Hello</p>
+      <AceEditor
+        mode="java"
+        theme="github"
+        onChange={onChange}
+        name="editor"
+        editorProps={{ $blockScrolling: true }}
+      />
+      ,
+    </div>
+  );
 }
 
 export default App;
--- src/gpt3.js
+++ src/gpt3.js
@@ -1,41 +1,20 @@
 const { Configuration, OpenAIApi } = require("openai");
 
-export async function callGPT3(myPrompt, onResponse) {
+export async function callGPT3(myPrompt) {
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   });
   const openai = new OpenAIApi(configuration);
 
-  console.log("what is my prompt??");
-  console.log(myPrompt);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: myPrompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
 
-  openai
-    .createCompletion({
-      model: "text-davinci-003",
-      prompt: myPrompt,
-      temperature: 0.7,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: ["TA:"],
-    })
-    .then((response) => {
-      console.log("i'm here!");
-      console.log(response.data.choices[0].text);
-      if (response.data.choices[0].text.trim().replace(/[\n]/gm, "") == "") {
-        console.log("abort mission chief");
-        onResponse("[no response ...]");
-      } else if (
-        response.data.choices[0].text.trim().replace(/[\n]/gm, "") == "Sally:"
-      ) {
-        console.log("abort mission chief #2");
-        onResponse("[no response ...]");
-      } else {
-        onResponse(response.data.choices[0].text);
-      }
-    });
+  return response;
 }
-
-// need to have some logic for the case where we don't get any response back...
-// we need to just send the prompt again?
